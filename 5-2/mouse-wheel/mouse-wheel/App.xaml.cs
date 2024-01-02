81e227cb85e53de78759fc7d94840303fb241dd3
Hust15900 <1205778269@qq.com>
2024-01-02 19:15:53 +0800

    add file 5-2/

---
+++ 5-2/mouse-wheel/mouse-wheel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace mouse_wheel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 5-2/mouse-wheel/mouse-wheel/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mouse_wheel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // 设置全局变量
+        private double currentvalue;
+        // 定义增量
+        private double step;
+        private void textbox_1_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            textbox_1.Focus();
+            // 获取当前的textbox_1中的数值
+            currentvalue = double.Parse(textbox_1.Text);
+            // 步长
+            step = 1;
+            if (e.Delta > 0)
+            {
+                // 向上滚动时
+                // 实现增加1
+                currentvalue += step;
+            }
+            else if (e.Delta < 0)
+            {
+                currentvalue -= step;
+            }
+
+            // 设置TextBox的新值
+            textbox_1.Text = currentvalue.ToString();
+        }
+    }
+}
